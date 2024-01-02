fa910e5c26a83e75c8d17c5779df4aa31c365346
Parfene Catalin <=>
2024-01-02 10:57:22 +0200

    Added on delete cascade in Weather.java

--- src/main/java/org/example/hibernate/Model/Weather.java
+++ src/main/java/org/example/hibernate/Model/Weather.java
@@ -2,6 +2,8 @@ package org.example.hibernate.Model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -27,20 +29,9 @@ public class Weather {
     private long visibility;
     private int pressure;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "forecast_fk")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Forecast forecast;
-
-    public Weather(String code, LocalTime time, boolean isRaining, int windSpeed, int temperature, int cloudsPercentage, long visibility, int pressure) {
-        this.code = code;
-        this.time = time;
-        this.isRaining = isRaining;
-        this.windSpeed = windSpeed;
-        this.temperature = temperature;
-        this.cloudsPercentage = cloudsPercentage;
-        this.visibility = visibility;
-        this.pressure = pressure;
-    }
 
     public Weather(String code, LocalTime time, boolean isRaining, int windSpeed, int temperature, int cloudsPercentage, long visibility, int pressure, Forecast forecast) {
         this.code = code;
